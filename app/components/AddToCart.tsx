4384399adf558622b92d29984d4a8cc18346f611
wajarabhol <jeanswc1938@gmail.com>
2024-08-14 00:06:38 +0700

    create nextjs - close #1

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from "react";
+
+
+const AddToCart = () => {
+    return (
+        <div>
+            <button className='btn btn-secondary' onClick={() => console.log('Click')}>Add to cart</button>
+        </div>
+    )
+}
+
+export default AddToCart;
